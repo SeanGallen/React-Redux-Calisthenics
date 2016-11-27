ee1829aeea4b0f777f729eb33a441a71a3f00712
Sean <sjgallen@gmail.com>
2016-11-26 18:11:49 -0600

    added an index that renders a div/text

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return <div>What's up!</div>;
+}
+
+ReactDom.render(<App />, document.querySelector('.container'))
